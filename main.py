3dfacfa1c9748138e4a8bb2e7a476981055d17e1
starkoo1 <starkvaultprobot@gmail.com>
2025-06-27 23:20:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import logging
+from keep_alive import keep_alive
+keep_alive()
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto
 from telegram.ext import (
     ApplicationBuilder,
